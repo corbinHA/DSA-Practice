44b709abf3dd9925b76c7dfefcdf9cd9230d7055
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-09-30 16:08:40 -0500

    spelling changes

--- Algo-expert/Kadane's_Algorithm.py
+++ Algo-expert/Kadane's_Algorithm.py
@@ -2,7 +2,7 @@
 # 	Write a function that takes in a non-empty array of integers and returns the maximum sum that can be obtained by summing up all of the integers in a non-empty subarray of the input.
 #  A subarray must only contain adjacent numbers.
 
-# Smple imput: array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+# Sample input: array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
 # Output: 19 [1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1]
 
 
