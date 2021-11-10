78ff4c94451057ac056e09c2df12a3886cb257dd
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-11-10 15:53:01 -0600

    added better optimization of solution for circular print

--- Hackerrank/Circular_printer.js
+++ Hackerrank/Circular_printer.js
@@ -3,6 +3,7 @@ function cicularPrint(s) {
   let count = 0
   const string = s.toLowerCase().split('')
   const letterTable = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  // const letterTable = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   let forwardPoint = 0
   let backwardPoint = 0
   let stringPoint = 0
@@ -37,3 +38,33 @@ function cicularPrint(s) {
 
 
 console.log(cicularPrint('UXDHVJGKLRLEMCKASKLJHDFLAKHSDF'))
+
+
+// better optimization
+
+function shortestTime(str) {
+const alpha = buildLetterIndex();
+	let last = ‘A’;
+	let time = 0;
+	for (let c of str) {
+		time += getShortest(last, c, alpha);
+		last = c;
+	}
+	return time;
+}
+
+const buildLetterIndex = () => {
+	const alpha = “ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
+	const index = {};
+	for (let i = 0; i < alpha.length; i++) {
+index[alpha[i]] = i;
+}
+return index;
+}
+
+const getShortest = (c1,c2,alpha) => {
+	const i1 = alpha[c1];
+	const i2 = alpha[c2];
+	const dist = Math.abs(i1 - i2); 
+	return Math.min(dist, 26 - dist);
+}
