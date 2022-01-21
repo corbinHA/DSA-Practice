fb5c465df1fdfe198b6e4afb13f6710e3cdd27c7
Corbin Armendariz <corbinarmendariz@gmail.com>
2022-01-21 12:54:42 -0600

    added merge intervals

---
+++ Leetcode/#51 Merge Intervals.js
@@ -0,0 +1,16 @@
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const results = [intervals[0]];
+    
+    for (let interval of intervals) {
+      let e1 = results[results.length-1][1]
+      let s1 = interval[0]
+      let e2 = interval[1]
+      if (e1 >= s1) {
+        results[results.length - 1][1] = Math.max(e1 , e2);
+      } else {
+        results.push(interval)
+      }
+    }
+  return results
+};
